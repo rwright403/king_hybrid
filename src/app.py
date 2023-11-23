2dba127d864684f671bd0d90f9f96a740a2328c7
ryan <93813225+rwright808@users.noreply.github.com>
2023-11-22 19:47:08 -0800

    test
    test

--- src/app.py
+++ src/app.py
@@ -3,12 +3,31 @@
 from src.thrust_curve import thrust_curve
 from src.flight_sim import flight_sim
 
-#Thrust Curve and Flight Sim
-thrust_curve.run()
-flight_sim.run()
-
-#Validation script
-"""
-thrust_curve.run()
 
 """
+user_input = 0
+while(user_input !=0):
+    print("\n")
+    print("1 --> Thrust Curve")
+    print("2 --> Flight Sim")
+    print("3 --> Sensitivity Analysis")
+    print("4 --> Model Validation")
+
+    user_input = input("Enter number to select analysis:")
+
+if user_input =='1':
+    print("Running Thrust Curve from inputs in constants.py")
+    thrust_curve.run()
+
+if user_input =='2':
+    print("Running Flight Sim from inputs in constants.py")
+    thrust_curve.run()
+    flight_sim.run()
+
+if user_input =='3':
+    exit()
+
+if user_input == '4':
+    exit()
+
+    """
