6fbce21498db328b6ba94f7f9fb74e07a48f37a1
rwright <rwright2500@gmail.com>
2024-07-27 01:01:55 -0700

    fixed mistake implementing HEM model, continuing to debug

--- src/constants.py
+++ src/constants.py
@@ -26,20 +26,52 @@ rho_fuel = 900 # kg/m^3
 # RocketCEA doesnt have paraffin built in: CREATE IT BELOW
 #C32H66 from RPA Paraffin Wax Composition
 CEA_fuel_str = f"""
-fuel paraffin  C 20   H 52    wt%=100.00
-h,KJ/Kgmol=-529200.0    t(k)=298.15   rho,kg/m3={rho_fuel}
+fuel paraffin  C 20   H 42    wt%=100.00
+h,KJ/mol=-13313.49  t(k)=298.15   rho,kg/m3={rho_fuel}
 """
+
+#-13313.49
+# https://web.stanford.edu/~cantwell/AA283_Course_Material/AA283_Resources/References/paraffin%20heat%20of%20formation.pdf
 #previously using C 32 H 66, h,KJ/Kgmol=-1860600     t(k)=298.15   rho,kg/m3={rho_fuel}, it seems pretty insensitive to this
 
 a = 0.155/1000 #m/s
 n = 0.5
 
 P_atm = 101325 #Pa
+"""
+m_fuel_i = 6.173983 #kg
+L = 0.4 #m
+A_port_i = 0.001963495408 #m^2
+ 
+A_throat = 0.000697464985 #m^2
+A_exit = 0.00417781528 #m^2
+
+V_tank = 0.043 #m^3
+
+P_tank = 6.5e+6 #Pa
+m_ox = 18.30872404 #kg
+"""
+
+"""
+num_orifices
+Cd
+Orifice_diam
+
+"""
+C_inj = 0.00002243097155 #m^2 #(num_orifices * Cd * orifice_diam)
+
+
+### SELECT INJECTOR MODEL!
+# 1 --> SPI 
+# 2 --> HEM
+# 3 --> Dyer
+inj_model = 2
+
 
 ### Sim Variables
-timestep = 0.01 #s
+timestep = 0.05 #s
 all_error = 0.01 
-sim_time = 11 #s (time engine will be simulated over)
+sim_time = 9.048 #s (time engine will be simulated over)
 
 ###ROCKET DATA FOR TRAJECTORY SIM--> MVH-1
 rocket_fuselage_rad = intometer(5.5/2) #m --> for trajectory sim
@@ -60,7 +92,7 @@ model_p_cc_file_path = r'./src/p_cc.csv'
 model_p_tank_file_path = r'./src/p_tank.csv'
 
 ###Phoenix 1a
-"""
+
 m_fuel_i = 6.173983 #kg
 L = 0.4 #m
 A_port_i = 0.001963495408 #m^2
@@ -78,16 +110,17 @@ C_inj = 0.00002243097155 #m^2 (number of orifices * Cd * Orifice Area)
 exp_thrust_file_path = r'./src/bens_validation_data/Phoenix_1a/Phoenix_1a_Thrust.csv'
 exp_p_cc_file_path = r'./src/bens_validation_data/Phoenix_1a/Phoenix_1a_CC_Pressure.csv'
 exp_p_tank_file_path = r'./src/bens_validation_data/Phoenix_1a/Phoenix_1a_Tank_Pressure.csv'
-"""
+
+
 
 ###Deliverance II
-"""
+
 m_fuel_i = 1.5 #kg
 L = 0.3852333 #m
 A_port_i = 0.0038319753 #m^2
 
 A_throat = 0.0010653525 #m^2
-A_exit = 0.0054347544 #m^2
+A_exit = 0.00531921243 #m^2
 
 V_tank = 0.012 #m^3
 
@@ -98,9 +131,10 @@ C_inj = 0.0000294229787 #m^2
 exp_thrust_file_path = r'./src/bens_validation_data/UofT_Deliverance_II/UofT_Deliverance_II_Thrust.csv'
 exp_p_cc_file_path = r'./src/bens_validation_data/UofT_Deliverance_II/UofT_Deliverance_II_CC_Pressure.csv'
 exp_p_tank_file_path = r'./src/bens_validation_data/UofT_Deliverance_II/UofT_Deliverance_II_Tank_Pressure.csv'
-"""
+
 
 ###Boundless
+"""
 m_fuel_i = 5.44311 #kg
 L = 0.41130982 #m
 A_port_i = 0.001302041 #m^2
@@ -114,10 +148,10 @@ P_tank = 4933902 #4.206e6 #Pa
 m_ox = 14.61751413 #kg
 C_inj = 0.00002364094414 #m^2
 
-
 exp_thrust_file_path = r'./src/bens_validation_data/UofW_Boundless/UofW_Boundless_Thrust.csv'
 exp_p_cc_file_path = r'./src/bens_validation_data/UofW_Boundless/UofW_Boundless_CC_Pressure.csv'
 exp_p_tank_file_path = r'./src/bens_validation_data/UofW_Boundless/UofW_Boundless_Tank_Pressure.csv'
+"""
 
 
 ###SENSITIVITY ANALYSIS INFORMATION!!!! (only works for engine rn)
@@ -137,6 +171,6 @@ A_exit
 """
 
 test_var = "C_inj"
-min_bound = 1e-5
-max_bound = 5e-5
-num_iterations=5
+min_bound = 1.71e-5
+max_bound = 2.39e-5
+num_iterations=3
