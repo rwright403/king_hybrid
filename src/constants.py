57a4315c46c0fa752c1b5b077bbf3d445e18bcd4
rwright <rwright2500@gmail.com>
2024-07-26 20:12:53 -0700

    started implementing HEM model, looks good so far, still some next steps but getting close "serious but doable amount of work" --> HYBRID 2024

--- src/constants.py
+++ src/constants.py
@@ -19,8 +19,6 @@ hour = 13
 
 ###ENGINE DATA
 oxName = 'N2O'
-rho_ox_liq = 1220 #kg/m^3
-rho_ox_gas = 1.9277 #kg/m^3
 
 fuelName = 'paraffin'
 rho_fuel = 900 # kg/m^3
@@ -31,25 +29,10 @@ CEA_fuel_str = f"""
 fuel paraffin  C 20   H 52    wt%=100.00
 h,KJ/Kgmol=-529200.0    t(k)=298.15   rho,kg/m3={rho_fuel}
 """
+#previously using C 32 H 66, h,KJ/Kgmol=-1860600     t(k)=298.15   rho,kg/m3={rho_fuel}, it seems pretty insensitive to this
 
-#previously using C 32 H 66, h,KJ/Kgmol=-1860600     t(k)=298.15   rho,kg/m3={rho_fuel}
-
-m_fuel_i = 5.44311 #kg
 a = 0.155/1000 #m/s
 n = 0.5
-L = 0.41130982 #m
-A_port_i = 0.001302041 #m^2
- 
-A_throat = 0.001302041 #m^2
-A_exit = 0.0061470149 #m^2
-
-
-V_tank = 0.024956005 #m^3
-
-
-P_tank = 4933902 #4.206e6 #Pa
-fill_level = 0.8#0.53562755175
-C_inj = 2.626771572e-5 #0.00002364094414 
 
 P_atm = 101325 #Pa
 
@@ -58,7 +41,7 @@ timestep = 0.01 #s
 all_error = 0.01 
 sim_time = 11 #s (time engine will be simulated over)
 
-###ROCKET DATA --> MVH-1
+###ROCKET DATA FOR TRAJECTORY SIM--> MVH-1
 rocket_fuselage_rad = intometer(5.5/2) #m --> for trajectory sim
 rocket_dry_mass = 30 #kg
 
@@ -77,24 +60,70 @@ model_p_cc_file_path = r'./src/p_cc.csv'
 model_p_tank_file_path = r'./src/p_tank.csv'
 
 ###Phoenix 1a
-#exp_thrust_file_path = r'./src/bens_validation_data/Phoenix_1a/Phoenix_1a_Thrust.csv'
-#exp_p_cc_file_path = r'./src/bens_validation_data/Phoenix_1a/Phoenix_1a_CC_Pressure.csv'
-#exp_p_tank_file_path = r'./src/bens_validation_data/Phoenix_1a/Phoenix_1a_Tank_Pressure.csv'
+"""
+m_fuel_i = 6.173983 #kg
+L = 0.4 #m
+A_port_i = 0.001963495408 #m^2
+ 
+A_throat = 0.000697464985 #m^2
+A_exit = 0.00417781528 #m^2
+
+V_tank = 0.043 #m^3
+
+P_tank = 6.5e+6 #Pa
+m_ox = 18.30872404 #kg
+C_inj = 0.00002243097155 #m^2 (number of orifices * Cd * Orifice Area)
+
+
+exp_thrust_file_path = r'./src/bens_validation_data/Phoenix_1a/Phoenix_1a_Thrust.csv'
+exp_p_cc_file_path = r'./src/bens_validation_data/Phoenix_1a/Phoenix_1a_CC_Pressure.csv'
+exp_p_tank_file_path = r'./src/bens_validation_data/Phoenix_1a/Phoenix_1a_Tank_Pressure.csv'
+"""
 
 ###Deliverance II
-#exp_thrust_file_path = r'./src/bens_validation_data/UofT_Deliverance_II/UofT_Deliverance_II_Thrust.csv'
-#exp_p_cc_file_path = r'./src/bens_validation_data/UofT_Deliverance_II/UofT_Deliverance_II_CC_Pressure.csv'
-#exp_p_tank_file_path = r'./src/bens_validation_data/UofT_Deliverance_II/UofT_Deliverance_II_Tank_Pressure.csv'
+"""
+m_fuel_i = 1.5 #kg
+L = 0.3852333 #m
+A_port_i = 0.0038319753 #m^2
+
+A_throat = 0.0010653525 #m^2
+A_exit = 0.0054347544 #m^2
+
+V_tank = 0.012 #m^3
+
+P_tank = 3920207.656 #Pa
+m_ox = 8.2 #kg
+C_inj = 0.0000294229787 #m^2
+
+exp_thrust_file_path = r'./src/bens_validation_data/UofT_Deliverance_II/UofT_Deliverance_II_Thrust.csv'
+exp_p_cc_file_path = r'./src/bens_validation_data/UofT_Deliverance_II/UofT_Deliverance_II_CC_Pressure.csv'
+exp_p_tank_file_path = r'./src/bens_validation_data/UofT_Deliverance_II/UofT_Deliverance_II_Tank_Pressure.csv'
+"""
 
 ###Boundless
+m_fuel_i = 5.44311 #kg
+L = 0.41130982 #m
+A_port_i = 0.001302041 #m^2
+ 
+A_throat = 0.001302041 #m^2
+A_exit = 0.0061470149 #m^2
+
+V_tank = 0.024956005 #m^3
+
+P_tank = 4933902 #4.206e6 #Pa
+m_ox = 14.61751413 #kg
+C_inj = 0.00002364094414 #m^2
+
+
 exp_thrust_file_path = r'./src/bens_validation_data/UofW_Boundless/UofW_Boundless_Thrust.csv'
 exp_p_cc_file_path = r'./src/bens_validation_data/UofW_Boundless/UofW_Boundless_CC_Pressure.csv'
 exp_p_tank_file_path = r'./src/bens_validation_data/UofW_Boundless/UofW_Boundless_Tank_Pressure.csv'
 
+
 ###SENSITIVITY ANALYSIS INFORMATION!!!! (only works for engine rn)
 """
 Variables to analyize:
-fill_level
+fill_level TODO: update in sensitivity analysis
 C_inj
 V_tank
 P_tank
