d57bbd2cab1799651ea631ad08e3c2647f6b7353
rwright <rwright2500@gmail.com>
2024-10-07 22:15:29 -0700

    still running

--- src/testing_HEM_fix.py
+++ src/testing_HEM_fix.py
@@ -1,5 +1,6 @@
 import numpy as np
 import CoolProp.CoolProp as CP
+import matplotlib.pyplot as plt
 
 #testing hem model fix
 
@@ -16,7 +17,7 @@ m_dot_ox = None
 s_inj = CP.PropsSI('S', 'H', h_tank_exit, 'P', P_tank, 'N2O')
 h_inj_exit = CP.PropsSI('H', 'S', s_inj, 'P', P_cc, 'N2O')
             
-rho_exit = CP.PropsSI('D', 'S', s_inj, 'P', P_cc, 'N2O')
+#rho_exit = CP.PropsSI('D', 'S', s_inj, 'P', P_cc, 'N2O')
 #m_dot_ox  = Cd_1 * A_inj_1 * rho_exit * np.sqrt( 2 * (h_tank_exit -  h_inj_exit) )
 
 
@@ -43,3 +44,16 @@ else:
 #print(m_dot_ox, rho_exit, "choked rho: ", CP.PropsSI('D', 'S', s_inj, 'P', P_crit, 'N2O'), "unchoked rho: ", CP.PropsSI('D', 'S', s_inj, 'P', P_cc, 'N2O'))
 #print("h --> f(P_cc): ", h_inj_exit, "h --> f(P_crit): ", CP.PropsSI('H', 'S', s_inj, 'P', P_crit, 'N2O') )
 
+"""
+delta_p_arr = []
+for p in downstream_pres_arr:
+    delta_p_arr.append(P_tank-p)
+
+
+plt.plot(delta_p_arr,m_dot_HEM_arr)
+plt.xlabel('delta P (MPa)')
+plt.ylabel('Mass Flow Rate (kg/s)')
+plt.title('delta P (MPa) vs Mass Flow Rate (kg/s)')
+plt.grid(True)
+plt.show()
+"""
