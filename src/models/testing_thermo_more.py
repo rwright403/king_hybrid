a381f2766dbabad4faa2370715e8f085c4fec78e
rwright <rwright2500@gmail.com>
2024-12-14 14:22:32 -0800

    14-dec-2024, 2:16pm. In the past week I learned that I was inappropriately using coolprop, which does not account for the metastable states this script is supposed to model. The fix is to use coolprop, so I am uploading this as a fallback incase the changes I am going to make will accidentally break the script.  - maybe i shouldn't add this as well, but I have recieved a lot of negative feedback in general this year. I have definitely made a lot of mistakes here with the script, but I am trying (sometimes not as well as I should) and learning (sometimes slowly.) There are things I feel very strongly about, and one thing is putting in the time at the start when the design is less fixed. I have spent more time than I should have second guessing that. I believe this is going to pay off, and even if it doesn't, I had a lot of fun working on it. Some of the feedback was definitely really helpful and I acknowledge that, but some of it was not as constructive as it could have been. - this is all to say I don't care

---
+++ src/models/testing_thermo_more.py
@@ -0,0 +1,29 @@
+from thermo.eos import PR
+import CoolProp.CoolProp as CP
+
+# Define inputs
+T = 290  # Temperature in Kelvin
+P = 5e6  # Pressure in Pascals (5 MPa)
+
+# Critical properties for the fluid (example: nitrous oxide)
+Tc = 309.57  # Critical temperature in Kelvin
+Pc = 7.257e6  # Critical pressure in Pascals
+omega = 0.04  # Acentric factor
+
+molar_mass = 44.013/1000 # in kg/mol, for N2O it's ~44.013 g/mol
+
+#check phase with coolprop:
+#print(CP.PhaseSI('T', T, 'P', P, "N2O" ))
+# Initialize Peng-Robinson EOS
+eos = PR(T=T, P=P, Tc=Tc, Pc=Pc, omega=omega)
+print(eos.phase)
+
+
+#rho_vap = 1/eos.V_g * molar_mass  # Get the specific volume (m^3/mol)
+rho_liq = 1/eos.V_l  * molar_mass # Get the specific volume (m^3/mol)
+    
+
+
+print(f"At T = {T} K and P = {P / 1e6} MPa:")
+print(f"Liquid density: {rho_liq:.2f} kg/m³")
+#print(f"Vapor density: {rho_vap:.2f} kg/m³")
